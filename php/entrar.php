0ff3f83a78b4e6b26559b9038851b49c07804250
CarlosTakashi <carlostgt1590@gmail.com>
2024-05-27 21:06:35 -0300

    att

---
+++ php/entrar.php
@@ -0,0 +1,24 @@
+<?php
+
+    $nome = $_POST['nome'] ?? 'slk'; 
+    $senha = $_POST['senha'] ?? '';
+
+    include "conexao.php";
+
+    $sql = "SELECT email FROM usuario WHERE nome_user = '$nome' AND senha = '$senha'";
+
+    $dados = mysqli_query($con, $sql);
+
+    if (mysqli_num_rows($dados)>0) {
+        while ($linha = mysqli_fetch_assoc($dados)) {
+            foreach($linha as $key => $value){
+                $info = &$value;
+                echo "login realizado com sucesso";
+            }
+        }
+    }else{
+        echo "email ou senha invalido";
+    }
+ 
+
+?>
---
+++ php/login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleLogin.css">
+    <title>login</title>
+</head>
+<body>
+    <div class="wrapper">
+
+   
+         
+    <div class="text-center mt-4 name">
+            Rythim Game
+        </div>
+        <form class="p-3 mt-3" action="entrar.php" method="POST">
+            <div class="form-field d-flex align-items-center">
+                <span class="far fa-user"></span>
+                <input type="text" name="nome" id="userName" placeholder="Username" required>
+            </div>
+            <div class="form-field d-flex align-items-center">
+                <span class="fas fa-key"></span>
+                <input type="password" name="senha" id="pwd" placeholder="Password" required>
+            </div>
+            <button class="btn mt-3">Login</button>
+        </form>
+        <div class="text-center fs-6">
+            <a href="#">Forget password?</a> or <a href="cadastro.php">Sign up</a>
+        </div>
+
+    
+    </div>
+</body>
+</html>
