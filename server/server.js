4d88180890e4c909f2a82ba64964573ed3de52c6
Yume Fernandes <107142833+Dehrangerz9@users.noreply.github.com>
2024-06-21 17:41:01 +0000

    [FEAT] CRIAÇÃO DA TELA INDEX.EJS

--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ const { Pool } = require("pg");
 
 const app = express();
 
+// Configuração do motor de visualização EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, '../public')));
 
 const pool = new Pool({
   connectionString:
@@ -17,7 +21,7 @@ const pool = new Pool({
 });
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render('index', { title: 'Home', userName: 'Dehrangerz9',userLevel: 10,musicaNome: null }); // Renderizando o template 'index.ejs' com um título
 });
 
 app.get("/dados", async (req, res) => {
@@ -27,7 +31,7 @@ app.get("/dados", async (req, res) => {
     client.release();
     res.send(result.rows);
   } catch (err) {
-    console.error("Erro ao exercutar a busca", err);
+    console.error("Erro ao executar a busca", err);
     res.status(500).send("Erro ao executar a consulta");
   }
 });
